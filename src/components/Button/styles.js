9c4dffb567c736681adfc0e22592be5e70cfd1c0
bncblnc <bncblnc@gmail.com>
2023-08-15 16:53:11 -0300

    button and nav

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,30 +1,38 @@
-import styled, {css}  from 'styled-components';
+import styled, { css } from "styled-components";
 
 export const ButtonContainer = styled.button`
-    background: #565656;
-    border-radius: 22px;
-    position: relative;
+  background: ${({ variant }) =>
+    variant === "primary" ? "#565656" : "#e4105d"};
+  border: none;
+  border-radius: 22px;
+  position: relative;
 
-    color: #FFFFFF;
-    padding: 2px 12px;
-    min-width: 120px;
-    width: 100%;
-    
-    ${({variant}) => variant !== "primary" && css`
-        min-width: 167px;
-        height: 33px;
-        
-        background: #E4105D;
+  color: #ffffff;
+  padding: 5px 12px;
+  min-width: 120px;
+  width: 100%;
+  font-size: 16px;
+  cursor: pointer;
 
-        &::after {
-            content: '';
-            position: absolute;
-            border: 1px solid #E4105D;
-            top: -5px;
-            left: -6px;
-            width: calc(100% + 10px);
-            height: calc(100% + 10px);
-            border-radius: 22px;
-        }
+  &:hover {
+    opacity: 0.85;
+  }
+
+  ${({ variant }) =>
+    variant !== "primary" &&
+    css`
+      min-width: 167px;
+      height: 33px;
+
+      &::after {
+        content: "";
+        position: absolute;
+        border: 1px solid #e4105d;
+        top: -5px;
+        left: -6px;
+        width: calc(100% + 10px);
+        height: calc(100% + 10px);
+        border-radius: 22px;
+      }
     `}
-`
+`;
