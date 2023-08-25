c3f79f4439b102b6fea13173ec43468730c7096e
bncblnc <bncblnc@gmail.com>
2023-08-25 18:18:25 -0300

    userinfo ts

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import { Container, NameText, Progress, UserPicture } from "./styles";
+import { IUserInfo } from "./types";
 
-import { Container, NameText, Progress, UserPicture} from './styles';
-
-const UserInfo = ({nome,image, percentual}) => {
+const UserInfo = ({ nome, image, percentual }: IUserInfo) => {
   return (
     <Container>
-        <UserPicture src={image} />
-        <div>
-            <NameText>{nome}</NameText>
-            <Progress percentual={percentual} />
-        </div>
+      <UserPicture src={image} />
+      <div>
+        <NameText>{nome}</NameText>
+        <Progress percentual={percentual} />
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export { UserInfo }
+export { UserInfo };
